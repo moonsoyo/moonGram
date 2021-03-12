710777d5241fabe403af4a8a1ed6033f6d3dc023
Soyoung Moon <moonsoyo@usc.edu>
2021-03-12 11:54:19 +0900

    Created a simple log in page

--- apis/views.py
+++ apis/views.py
@@ -1,5 +1,11 @@
 from django.http import JsonResponse
 from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.core.validators import validate_email, ValidationError
+from django.contrib.auth import authenticate, login
 
 # BaseView class helps exchange JSON data when developing apis.
 # If LogIn, LogOut, Sign up etc views inherit BaseView, JSON data can be exchanged.
@@ -14,15 +20,43 @@ class BaseView(View):
 
 
 class UserCreateView(BaseView):
-    @method-decorator(csrf_exempt)
+    @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(UserCreateView, self).dispatch(request, *args, **kwargs)
 
     def post(self, request):
         username = request.POST.get('username', '')
+        if not username:
+            return self.response(message='Please put in your ID.', status=400)
         password = request.POST.get('password', '')
+        if not password:
+            return self.response(message='Please put in your password.', status=400)
         email = request.POST.get('email', '')
+        try:
+            validate_email(email)
+        except ValidationError:
+            self.response(message='Please put in your email in correct form.', status=400)
 
-        user = User.objects.create_user(username, email, password)
+        try:
+            user = User.objects.create_user(username, email, password)
+        except IntegrityError:
+            return self.response(message='This ID already exists.', status=400)
 
         return self.response({'user.id': user.id})
+
+
+class UserLoginView(BaseView):
+    def post(self, request):
+        username = request.POST.get('username', '')
+        if not username:
+            return self.response(message='Please put in the username', status=400)
+        password = request.POST.get('password', '')
+        if not password:
+            return self.response(message='Please put in the password', status=400)
+                
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return self.response(message='This user does not exist', status=400)
+        login(request, user)
+
+        return self.response
